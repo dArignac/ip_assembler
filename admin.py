078ba1a934c973f73925dcace831a79fbf243ac6
Alexander Herrmann <darignac@googlemail.com>
2013-07-19 19:42:15 +0200

    refactored the logic of bulk creating IPs and unifying them, refs #337

--- admin.py
+++ admin.py
@@ -27,23 +27,16 @@ class IPAdmin(admin.ModelAdmin):
         if request.method == 'POST':
             form = IPBatchMergeForm(request.POST)
             if form.is_valid():
-                # add IPs
 
                 def filter_ips(ip):
                     return len(ip) > 0
 
                 ips = filter(filter_ips, form.cleaned_data['ips'].split('\r\n'))
                 ip_count_before = IP.objects.count()
-                merged_ip_count = 0
 
                 if len(ips) > 0:
-                    ips_created = 0
-                    # for each ip, check if already existent, if not add
-                    for ip in ips:
-                        (s0, s1, s2, s3) = ip.split('.')
-                        (ip_db, is_ip_created) = IP.objects.get_or_create(seg_0=s0, seg_1=s1, seg_2=s2, seg_3=s3, )
-                        if is_ip_created:
-                            ips_created += 1
+                    # create the ips that are not already in database
+                    ips_created = IP.batch_add_ips(ips)
 
                     # display an info
                     info_text = _('%(ip_count)d IPs were given,<br /> %(ips_created)d IPs were created' % {'ip_count': len(ips), 'ips_created': ips_created})
@@ -51,39 +44,7 @@ class IPAdmin(admin.ModelAdmin):
                 # unify them
                 clean_ips = form.cleaned_data['show_cleaned_list']
                 if clean_ips:
-                    # store the ips that have already been processed
-                    processed_ips = []
-
-                    # iterate all ips, might take a while ;-)
-                    for ip in IP.objects.all().order_by('seg_0', 'seg_1', 'seg_2', 'seg_3'):
-                        # if the ip was processed, skip
-                        if ip in processed_ips:
-                            continue
-
-                        # get all siblings of the ip
-                        siblings = IP.objects.filter(seg_0=ip.seg_0, seg_1=ip.seg_1, seg_2=ip.seg_2, )\
-                            .exclude(pk=ip.pk)\
-                            .order_by('seg_0', 'seg_1', 'seg_2', 'seg_3')
-
-                        # there are siblings, process...
-                        if siblings.count() > 0:
-                            # check if there is already a starred IP in siblings
-                            try:
-                                starred_ip = siblings.get(seg_3='*')
-                            except:
-                            # no starred ip, create one
-                                if ip.seg_3 != '*':
-                                    starred_ip = IP.objects.create(seg_0=ip.seg_0, seg_1=ip.seg_1, seg_2=ip.seg_2, seg_3='*', )
-                                else:
-                                    starred_ip = ip
-
-                            # delete the others
-                            for sib_ip in siblings.exclude(pk=starred_ip.pk):
-                                processed_ips.append(sib_ip)
-                                merged_ip_count += 1
-                                sib_ip.delete()
-
-                        processed_ips.append(ip)
+                    merged_ip_count = IP.unify_ips()
 
                     info_text += ',<br /> ' + _('%(merged_ip_count)d IPs merged' % {'merged_ip_count': merged_ip_count})
                     info_text += ',<br /> ' + _('%(ip_count_before)d IPs before' % {'ip_count_before': ip_count_before})
--- models.py
+++ models.py
@@ -15,6 +15,75 @@ class IP(models.Model):
     seg_2 = models.CharField(max_length=3, verbose_name=_('Segment 3'))
     seg_3 = models.CharField(max_length=3, verbose_name=_('Segment 4'))
 
+    @staticmethod
+    def batch_add_ips(ips):
+        """
+        Adds the given list of IPs to the database if the IP is not already there.
+        :param ips: list of IPs
+        :return: number of created IPs
+        :type ips: list
+        :rtype: int
+        """
+        ips_created = 0
+        if len(ips) > 0:
+            # for each ip, check if already existent, if not add
+            for ip in ips:
+                (s0, s1, s2, s3) = ip.split('.')
+                (ip_db, is_ip_created) = IP.objects.get_or_create(seg_0=s0, seg_1=s1, seg_2=s2, seg_3=s3, )
+                if is_ip_created:
+                    ips_created += 1
+        return ips_created
+
+    @staticmethod
+    def unify_ips():
+        """
+        Unifies the currently saved IPs.
+        Unification is based on last IP segment.
+        So if there are is e.g. 192.168.128.121 and 192.168.128.122 tthey will be merged to 192.168.128.121.
+        This is a little aggressive but the spammers are aggressive, too.
+        :return: number of merged ips
+        :rtype: int
+        """
+        # TODO this needs performance improvement!
+        # store the ips that have already been processed
+        processed_ips = []
+
+        # number of merged ips
+        merged_ip_count = 0
+
+        # iterate all ips, might take a while ;-)
+        for ip in IP.objects.all().order_by('seg_0', 'seg_1', 'seg_2', 'seg_3'):
+            # if the ip was processed, skip
+            if ip in processed_ips:
+                continue
+
+            # get all siblings of the ip
+            siblings = IP.objects.filter(seg_0=ip.seg_0, seg_1=ip.seg_1, seg_2=ip.seg_2, )\
+                .exclude(pk=ip.pk)\
+                .order_by('seg_0', 'seg_1', 'seg_2', 'seg_3')
+
+            # there are siblings, process...
+            if siblings.count() > 0:
+                # check if there is already a starred IP in siblings
+                try:
+                    starred_ip = siblings.get(seg_3='*')
+                except:
+                # no starred ip, create one
+                    if ip.seg_3 != '*':
+                        starred_ip = IP.objects.create(seg_0=ip.seg_0, seg_1=ip.seg_1, seg_2=ip.seg_2, seg_3='*', )
+                    else:
+                        starred_ip = ip
+
+                # delete the others
+                for sib_ip in siblings.exclude(pk=starred_ip.pk):
+                    processed_ips.append(sib_ip)
+                    merged_ip_count += 1
+                    sib_ip.delete()
+
+            processed_ips.append(ip)
+
+        return merged_ip_count
+
     def __unicode__(self):
         return u'%s.%s.%s.%s' % (self.seg_0, self.seg_1, self.seg_2, self.seg_3)
 
