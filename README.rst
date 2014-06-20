42930ff22f2bce30b3a5b2a2731aad8738491e03
Alexander Herrmann <darignac@googlemail.com>
2014-06-20 15:02:03 +0200

    no additional info was created

---
+++ README.rst
@@ -0,0 +1,4 @@
+ip_assembler
+============
+
+What does this thing even do?
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='ip_assembler',
-    description='TODO',
+    description='Banned IP processing for .htaccess locations like Wordpress.',
     version='0.0.1',
     author='Alexander Herrmann',
     author_email='darignac@gmail.com',
     license='MIT',
     url='https://github.com/dArignac/ip_assembler',
     packages=['ip_assembler'],
-    # long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         'Django>=1.6,<1.7',
         'django-shared',
